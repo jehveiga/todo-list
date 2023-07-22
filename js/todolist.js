353220e0f5ba2a20ed9a16b4b467462d09ca6d5a
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-07-22 15:12:10 -0300

    Consumindo uma estrutura de dados para adicionar dinamicamente a task na ul

--- js/todolist.js
+++ js/todolist.js
@@ -5,32 +5,61 @@
     const itemInput = document.getElementById("item-input")
     const todoAddForm = document.getElementById("todo-add")
     const ulTodoList = document.getElementById("todo-list")
-    const lisTodoList = document.getElementsByTagName("li")
+    //const lisTodoList = document.getElementsByTagName("li")
 
+    let arrTasks = [
+        {
+            name: "task 1",
+            createAt: Date.now(),
+            completed: false,
+        },
+        {
+            name: "task 2",
+            createAt: Date.now(),
+            completed: false,
+        }
+    ]
+
+    // Função responsável por adicionar um evento de escuta de click no elemento li passado por parametro
     function addEventList(li) {
         li.addEventListener("click",function () {
             console.log(this)
-            console.log(this.textContent)
-            console.log(this.innerText)
-            console.log(this.innerHTML)
-            console.log(this.outerHTML)
+
         })
     }
 
-    function addTasks(task) {
+    // Função responsável por criar o elemento li e paragrafo e adicionar os dados do objeto recebido por parametro no paragrafo
+    function generateLiTask(obj){
         const li = document.createElement("li")
         li.className = "todo-item"
         const paragrafo = document.createElement("p")
         paragrafo.className = "task-name"
-        paragrafo.textContent = task
+        paragrafo.textContent = obj.name
         li.appendChild(paragrafo)
-        ulTodoList.appendChild(li)
-
         addEventList(li)
+
+        return li
     }
 
+    // Função responsavel por limpar a ul e adicionar a renderização dos dados na ul list atualizando o HTML da página
+    // adicionando a li retornado da função generateLiTask()
+    function renderTasks(){
+        ulTodoList.innerHTML = ""
+        arrTasks.forEach(taskObj => {
+            ulTodoList.appendChild(generateLiTask(taskObj))
+        });
+    }
 
-    // Adicionando a escuta do evento de submit para adicionar a Todo criada na ul
+    // Função responsavel por adicionar no array de objetos a task passada no input
+    function addTasks(task) {
+        arrTasks.push({
+            name: task,
+            createAt: Date.now(),
+            completed: false,
+        })
+    }
+
+    // Adicionando a escuta do evento de submit para adicionar a To do criada na ul
     todoAddForm.addEventListener("submit", function (e) {
         e.preventDefault()
         console.log(itemInput.value)
@@ -40,12 +69,12 @@
         //     </li>
         // `
         addTasks(itemInput.value)
+        renderTasks()
+
         itemInput.value = "";
         itemInput.focus()
     });
 
-    [...lisTodoList].forEach(li => {
-        addEventList(li)
-    });
+    renderTasks()
 
 })();
